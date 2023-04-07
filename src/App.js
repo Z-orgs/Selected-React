a441e853e1c52898f092da1e9939180634f1f405
A$PIRE mxz <60683892+mxzxm@users.noreply.github.com>
2023-04-07 20:33:35 +0700

    delete src

--- src/App.js
+++
@@ -1,9 +0,0 @@
-import logo from './logo.svg';
-import './App.css';
-import axios from 'axios';
-import Login from './login';
-import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
-
-function App() {}
-
-export default App;
