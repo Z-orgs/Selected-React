80794f44e444ac509d433f1dfc3cb85b91c8745f
BLUE <89306062+BLVEhv@users.noreply.github.com>
2023-04-21 03:36:15 +0700

    reset password artist

--- src/pages/artist/ListArtistPage.js
+++ src/pages/artist/ListArtistPage.js
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getAllArtist } from "../../redux/apiRequest";
 import { NavLink } from 'react-router-dom';
+import axios from "axios";
 
 const ListArtistPage = () => {
     const admin = useSelector((state) => state.auth.login?.currentUser);
@@ -11,13 +12,28 @@ const ListArtistPage = () => {
         getAllArtist(admin?.data?.admin_token, dispatch);
     }, []);
     localStorage.setItem("token", admin?.data?.admin_token);
+    const handleResetPassword = async (id) => {
+        try {
+            const res = await axios.put(`http://localhost:3000/artist/reset/${id}`,
+                { id },
+                {
+                    headers: { Authorization: `Bearer ${admin?.data?.admin_token}` },
+                });
+        } catch (e) {
+            alert(e);
+        }
+
+    };
     return (
         <>
             <div>
                 {listArtist &&
                     listArtist.map((art) => (
                         <div key={art._id}>
-                            {art.username} - {art.nickName}
+                            {art.username} - {art.nickName} &nbsp;
+                            <button onClick={() => handleResetPassword(art._id)} >
+                                Reset password
+                            </button>
                         </div>
                     ))}
             </div>
